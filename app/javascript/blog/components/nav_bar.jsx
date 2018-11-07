8b2c48602cc7259cd0d3387e85e62cb2a4fa7ac6
elijah johnston <eli@millinno.com>
2018-11-07 15:23:08 +0100

    basic user flows init

--- app/javascript/blog/components/nav_bar.jsx
+++ app/javascript/blog/components/nav_bar.jsx
@@ -15,13 +15,6 @@ export default class NavBar extends Component {
             </Navbar.Brand>
           </Navbar.Header>
           <Nav>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3} to="/ideas/new">Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
-            </NavDropdown>
             <NavItem eventKey={1} data-method="delete" href="/users/sign_out">
               Logout
             </NavItem>
@@ -36,3 +29,12 @@ export default class NavBar extends Component {
     );
   }
 }
+
+
+            // <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            //   <MenuItem eventKey={3.1}>Action</MenuItem>
+            //   <MenuItem eventKey={3.2}>Another action</MenuItem>
+            //   <MenuItem eventKey={3.3} to="/ideas/new">Something else here</MenuItem>
+            //   <MenuItem divider />
+            //   <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            // </NavDropdown>
