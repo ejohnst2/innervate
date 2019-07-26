49bff3564d7ab29584aa3ad25b1d76c2667c54c4
elijah johnston <eli@millinno.com>
2019-07-26 20:29:03 +0800

    2 basic unit tests in for each model with fixtures to test

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
   test "a team must have a Slack ID" do
-    assert true
+    team = teams(:invalid_team)
+    assert_not team.valid?
   end
 
   test "a team must have a team plan" do
-    assert true
+    team = teams(:no_plan_team)
+    assert_not team.valid?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "a user must belong to a Slack team" do
-    assert true
+    user = users(:bob)
+    assert_not user.valid?
   end
 
   test "a user must have email, uid, first_name, and last_name" do
-    assert true
+    user = users(:karina)
+    assert_not user.valid?
   end
 end
