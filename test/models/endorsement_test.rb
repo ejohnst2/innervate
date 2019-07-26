9dc54b8083f12cfee433038aefa7d2f517c4b5b1
elijah johnston <eli@millinno.com>
2019-07-26 20:18:17 +0800

    basic tests for endorsements written

--- test/models/endorsement_test.rb
+++ test/models/endorsement_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 class EndorsementTest < ActiveSupport::TestCase
   test "idea must be present for endorsement" do
-    assert true
+    endorsement = endorsements(:no_idea_endorsement)
+    assert_not endorsement.valid?
   end
 
   test "user must be present for endorsement" do
-    assert true
+    endorsement = endorsements(:no_user_endorsement)
+    assert_not endorsement.valid?
   end
 end
